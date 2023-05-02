86a67357ee1cb32d70e8224b95d58c4620a4b0de
almaz4444 <almaz4444f@gmail.com>
2023-05-02 15:31:23 +0500

    Removed unnecessary files

--- Player.ino
+++
@@ -1,6 +0,0 @@
-void PlayerMove(const float xAxis, const float yAxis) {
-  if(!((getWall(byte(x - playerSize) >> BitTile, byte(y) >> BitTile).isWall && xAxis > 0) ||
-     (getWall(byte(x + playerSize) >> BitTile, byte(y) >> BitTile).isWall && xAxis < 0))) x -= xAxis;
-  if(!((getWall(byte(x) >> BitTile, byte(y - playerSize) >> BitTile).isWall && yAxis > 0) ||
-     (getWall(byte(x) >> BitTile, byte(y + playerSize) >> BitTile).isWall && yAxis < 0))) y -= yAxis;
-}
